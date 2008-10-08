576a8f8a27e4056fd9b1dafcf12367aa307ad65c
nighthawkthesane <nighthawkthesane@acd137fc-fe2b-0410-967f-fda31858317f>
2008-10-08 18:43:12 +0000

    StableBoy: - Removed some no longer used constants - Added a mountBypass table for special casing obnoxious mounts. I'm looking at you, Winged Steed of the Ebon Drake - Update parse mounts to check the mountBypass table - Finally remove deprecated functions (NorthrendFlyable() and InitializeMounts(), I don't think I'll need them any more - Use IsOutdoors() instead of SecureCmdOptionsParse in the ClickHandler - add a Shift-key short circuit to the Click handler to force using the ground mount - Add some comments
    git-svn-id: http://asylumsw.googlecode.com/svn/trunk/wow/StableBoy@89 acd137fc-fe2b-0410-967f-fda31858317f

--- StableBoy.lua
+++ StableBoy.lua
@@ -1,27 +1,32 @@
 local MOUNT_GROUND = 1
 local MOUNT_FLYING = 2
-local SPEED_SLOW = 1
-local SPEED_MEDIUM = 2
-local SPEED_FAST = 3
-local CONDITION_MOUNTED = '[mounted]'
-local CONDITION_MOUNTABLE = '[outdoors]'
-local CONDITION_FLYABLE = '[flyable]'
+local SPEED_SLOW = 1 -- 60% (Ground/Flying)
+local SPEED_MEDIUM = 2 -- 100% (Ground) / 280% (Flying)
+local SPEED_FAST = 3 -- 310% (Flying)
 BINDING_HEADER_STABLEBOY = 'StableBoy'
 BINDING_NAME_STABLEBOY_MOUNT_BEST = 'Summon Best Mount'
 BINDING_NAME_STABLEBOY_MOUNT_GROUND = 'Summon Ground Mount'
 
-CreateFrame("GameTooltip","StableBoyTooltip",UIParent,"GameTooltipTemplate")
-StableBoyTooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
-
 local function announce(msg)
 	DEFAULT_CHAT_FRAME:AddMessage("StableBoy: "..msg)
 end
+
+-- This table is for special-casing certain mounts that won't parse properly
+-- in the ParseMounts() method.
+local mountBypass = {
+	[54729] = { mountType=MOUNT_GROUND, speed=SPEED_MEDIUM } -- Winged Steed of the Ebon Blade
+}
+
 
 StableBoy = CreateFrame("frame", "StableBoyFrame", UIParent)
 StableBoy:SetScript("OnEvent", function(self, event, ...) return self[event](self, ...) end)
 StableBoy:RegisterEvent("ADDON_LOADED")
 StableBoy.myGroundMounts = {}
 StableBoy.myFlyingMounts = {}
+
+-- Create a scanning tooltip
+CreateFrame("GameTooltip","StableBoyTooltip",UIParent,"GameTooltipTemplate")
+StableBoyTooltip:SetOwner(WorldFrame, "ANCHOR_NONE")
 
 local menu = {
 	["Flying"] = {
@@ -39,7 +44,6 @@ local menu = {
 		submenu = {}
 	}
 }
-
 
 -- I could probably separate the various parts of this method into sub-methods
 function StableBoy:ADDON_LOADED(addon,...)
@@ -85,44 +89,48 @@ function StableBoy:ParseMounts()
 	local maxGroundSpeed = SPEED_SLOW
 	local maxFlyingSpeed = SPEED_SLOW
 	for i=1,maxMounts do
-		local thisMount = { mountType=MOUNT_GROUND, speed=SPEED_SLOW}
+		local thisMount
 		local creatureID,name,spellID = GetCompanionInfo('MOUNT',i)
-
-		StableBoyTooltip:SetHyperlink("spell:"..spellID)
-		local numLines = StableBoyTooltip:NumLines()
-		local text = ""
-		for j=1,numLines do
-			text = string.format("%s %s", text, _G["StableBoyTooltipTextLeft"..j]:GetText())
-		end
-
-		-- Determine if we're a flying mount.
-		-- Flying mounts can only be used in Outland or Northrend,
-		-- And say so on the tooltip.
-		if text:match("Outland") or text:match("Northrend") then
-			thisMount.mountType = MOUNT_FLYING
-		end
-
-		-- Figure out how fast this mount is.
-		if text:match("extremely%s+fast") then
-			-- "extremely fast" means it's a 310% Flying mount.
-			thisMount.speed = SPEED_FAST
-		elseif text:match("very%s+fast") then
-			-- "very fast" means it's a 100% speed ground mount, or a 280% speed flying mount.
-			thisMount.speed = SPEED_MEDIUM
-		end
-		
-		-- Update our max speed values if this is the fastest mount we've seen yet
-		if( thisMount.mountType == MOUNT_GROUND and thisMount.speed > maxGroundSpeed ) then
-			maxGroundSpeed = thisMount.speed
-		elseif( thisMount.mountType == MOUNT_FLYING and thisMount.speed > maxFlyingSpeed ) then
-			maxFlyingSpeed = thisMount.speed
-		end
-		
-		-- Add this mount to our menu
-		if( thisMount.mountType == MOUNT_FLYING ) then
-			menu["Flying"].submenu[i] = { text=name, value=i }
-		elseif( thisMount.mountType == MOUNT_GROUND ) then
-			menu["Ground"].submenu[i] = { text=name, value=i }
+		if( mountBypass[spellID] ) then
+			thisMount = mountBypass[spellID]
+		else
+			thisMount = { mountType=MOUNT_GROUND, speed=SPEED_SLOW}
+			StableBoyTooltip:SetHyperlink("spell:"..spellID)
+			local numLines = StableBoyTooltip:NumLines()
+			local text = ""
+			for j=1,numLines do
+				text = string.format("%s %s", text, _G["StableBoyTooltipTextLeft"..j]:GetText())
+			end
+	
+			-- Determine if we're a flying mount.
+			-- Flying mounts can only be used in Outland or Northrend,
+			-- And say so on the tooltip.
+			if text:match("Outland") or text:match("Northrend") then
+				thisMount.mountType = MOUNT_FLYING
+			end
+	
+			-- Figure out how fast this mount is.
+			if text:match("extremely%s+fast") then
+				-- "extremely fast" means it's a 310% Flying mount.
+				thisMount.speed = SPEED_FAST
+			elseif text:match("very%s+fast") then
+				-- "very fast" means it's a 100% speed ground mount, or a 280% speed flying mount.
+				thisMount.speed = SPEED_MEDIUM
+			end
+			
+			-- Update our max speed values if this is the fastest mount we've seen yet
+			if( thisMount.mountType == MOUNT_GROUND and thisMount.speed > maxGroundSpeed ) then
+				maxGroundSpeed = thisMount.speed
+			elseif( thisMount.mountType == MOUNT_FLYING and thisMount.speed > maxFlyingSpeed ) then
+				maxFlyingSpeed = thisMount.speed
+			end
+			
+			-- Add this mount to our menu
+			if( thisMount.mountType == MOUNT_FLYING ) then
+				menu["Flying"].submenu[i] = { text=name, value=i }
+			elseif( thisMount.mountType == MOUNT_GROUND ) then
+				menu["Ground"].submenu[i] = { text=name, value=i }
+			end
 		end
 		
 		myMounts[i] = thisMount
@@ -138,51 +146,15 @@ function StableBoy:ParseMounts()
 	end -- i,thisMount in myMounts
 end
 
-
--- Deprecated
-function StableBoy:InitializeMounts()
-	self.myFlyingMounts = {}
-	self.myGroundMounts = {}
-	
-	local myMounts = {}
-	local maxGroundSpeed = 0
-	local maxFlightSpeed = 0
-	local maxMounts = GetNumCompanions("MOUNT")
-
-	for i=1,maxMounts do
-		local creatureID,name,spellID = GetCompanionInfo("MOUNT",i)		
-		announce(i..":"..name..":"..spellID)
-		if( mounts[spellID] ) then
-			myMounts[spellID] = i
-			if( mounts[spellID].mountType == MOUNT_FLYING and mounts[spellID].speed >= maxFlightSpeed ) then
-				maxFlightSpeed = mounts[spellID].speed
-			elseif( mounts[spellID].mountType == MOUNT_GROUND and mounts[spellID].speed >= maxGroundSpeed ) then
-				maxGroundSpeed = mounts[spellID].speed
-			end
-		else
-			-- We don't know about the mount, /cry.
-			-- TODO: Try to parse the tooltip to figure out type/speed
-		end
-	end
-	
-	for spellID,cID in pairs(myMounts) do
-		if( mounts[spellID].mountType == MOUNT_FLYING and mounts[spellID].speed >= maxFlightSpeed ) then
-			self.myFlyingMounts[#self.myFlyingMounts+1] = cID
-		elseif( mounts[spellID].mountType == MOUNT_GROUND and mounts[spellID].speed >= maxGroundSpeed ) then
-			self.myGroundMounts[#self.myGroundMounts+1] = cID
-		end
-	end
-end
-
 function StableBoy:ClickHandler(forceGround)
 	if( UnitInVehicle("player") ) then
 		VehicleExit()
 	elseif( IsMounted() ) then
 		Dismount()
-	elseif( not InCombatLockdown() and SecureCmdOptionParse("[outdoors,nocombat]") ) then
+	elseif( not InCombatLockdown() and IsOutdoors() ) then
 		-- Only attempt to summon a flying mount if we HAVE flying mounts AND we're in a flyable zone
 		-- AND my hacky attempt to get around the fact that [flyable] doesn't work right in northrend
-		if( #self.myFlyingMounts > 0 and not forceGround and self:IsFlyableArea() ) then
+		if( #self.myFlyingMounts > 0 and not forceGround and not IsShiftKeyDown() and self:IsFlyableArea() ) then
 			self:SummonMount(self.myFlyingMounts)
 		else
 			self:SummonMount(self.myGroundMounts)
@@ -195,8 +167,10 @@ function StableBoy:SummonMount(mountList)
 	CallCompanion("MOUNT",mountList[random(#mountList)])
 end
 
--- This is (slightly) saner than NorthrendFlyable(), but it's a more full-on and
--- robust replacement for the global IsFlyableArea() method.
+-- A more robust replacement for the IsFlyableArea() method.
+-- This does some advanced checking, to handle the oddities of flying in
+-- northrend. It checks for Cold Weather Flying, whether your in Dalaran,
+-- and if you're in Krasus' Landing.
 function StableBoy:IsFlyableArea()
 	SetMapToCurrentZone()
 
@@ -217,26 +191,6 @@ function StableBoy:IsFlyableArea()
 	
 	-- We ARE in Krasus' Landing, we can fly.
 	return true;
-end
-
-
--- Deprecated
---
--- Hack of a function until they fix the [flyable] conditional in Northrend
--- Returns TRUE if the player is NOT in northrend, OR if the player knows Cold Weather Flying.
--- Returns false if the player IS in northrend AND does not know Cold Weather Flying
-function StableBoy:NorthrendFlyable()
-	-- Yes, this is a no-no, and an ugly hack, but I don't care because it'll work,
-	-- and I'm lazy. I may fix this, or I may just wait until [flyable] works and
-	-- I can ditch this entire function.
-	SetMapToCurrentZone()
-	
-	-- 54197 is the spelID of "Cold Weather Flying"	
-	if( GetCurrentMapContinent() ~= 4 or GetSpellInfo(GetSpellInfo(54197))) then
-		return true
-	else
-		return false
-	end
 end
 
 function StableBoy:LDB_OnClick(frame,button,down)
